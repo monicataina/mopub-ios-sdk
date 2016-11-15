b09f9087a7b6cb3cd95095290ee8f3e026fafa1a
monicataina <monicataina.marinescu@gameloft.com>
2016-11-15 16:50:25 +0200

    - update Facebook RewardedVideoCustomEvent (simplify the code)

--- AdNetworkSupport/Facebook/FacebookRewardedVideoCustomEvent.m
+++ AdNetworkSupport/Facebook/FacebookRewardedVideoCustomEvent.m
@@ -6,36 +6,34 @@
 //
 
 #import "FacebookRewardedVideoCustomEvent.h"
-#import "MPFacebookRouter.h"
 #import "MPRewardedVideoReward.h"
 #import "MPRewardedVideoError.h"
 #import "MPLogging.h"
+#import "MoPub.h"
 #import "FacebookInstanceMediationSettings.h"
-#import "MPInstanceProvider+Facebook.h"
+#include <FBAudienceNetwork/FBAudienceNetwork.h>
 
 static NSString *const kFacebookAdsOptionZoneIdKey = @"zoneId";
 
-@interface FacebookRewardedVideoCustomEvent () <MPFacebookRouterDelegate>
+@interface FacebookRewardedVideoCustomEvent () <FBRewardedVideoAdDelegate>
 
-@property (nonatomic, copy) NSString *zoneId;
+@property (nonatomic, assign) NSString *zoneId;
 
+@property (nonatomic, retain) FBRewardedVideoAd *loadingAd;
+@property (nonatomic, strong) NSString* rewardCurrencyForLoadingAd;
+@property (nonatomic, strong) NSNumber* rewardAmountForLoadingAd;
+
+@property (nonatomic, retain) FBRewardedVideoAd *displayedAd;
+@property (nonatomic, assign) NSString* rewardCurrencyForDisplayedAd;
+@property (nonatomic, assign) NSNumber* rewardAmountForDisplayedAd;
+
+@property (nonatomic, assign) BOOL isAdClosed;
+@property (nonatomic, assign) BOOL isRewardCallbackCalled;
 @end
 
 static int sFacebookIdentifier = -1;
 
 @implementation FacebookRewardedVideoCustomEvent
-
-+(void)initialize
-{
-    //force loading this class without -ObjC flag
-    [MPFacebookInstanceProvider initialize];
-}
-
-- (void)dealloc
-{
-    [[MPFacebookRouter sharedRouter] clearDelegate:self];
-    [super dealloc];
-}
 
 - (void)requestRewardedVideoWithCustomEventInfo:(NSDictionary *)info
 {
@@ -44,12 +42,48 @@ static int sFacebookIdentifier = -1;
     FacebookInstanceMediationSettings *settings = [self.delegate instanceMediationSettingsForClass:[FacebookInstanceMediationSettings class]];
     NSString *customerId = [self.delegate customerIdForRewardedVideoCustomEvent:self];
     
-    [[MPFacebookRouter sharedRouter] requestRewardedVideoAdWithZoneId:self.zoneId customerId:customerId settings:settings delegate:self];
+    static dispatch_once_t facebookInitToken;
+    dispatch_once(&facebookInitToken, ^{
+        if([[MoPub sharedInstance] m_enableDebugging])
+        {
+            //set the current device as test device for FAN ads
+            [FBAdSettings addTestDevice:[FBAdSettings testDeviceHash]];
+        }
+    });
+
+    NSString* userId = NULL;
+    if (customerId.length >0) {
+        userId = customerId;
+    } else if (settings.userIdentifier.length > 0) {
+        userId = settings.userIdentifier;
+    }
+    
+    self.rewardCurrencyForLoadingAd = kMPRewardedVideoRewardCurrencyTypeUnspecified;
+    if(settings.rewardCurrencyName.length > 0){
+        self.rewardCurrencyForLoadingAd = settings.rewardCurrencyName;
+    }
+    
+    self.rewardAmountForLoadingAd = @(kMPRewardedVideoRewardCurrencyAmountUnspecified);
+    if(settings.rewardAmount > 0){
+        self.rewardAmountForLoadingAd = [NSNumber numberWithInteger:settings.rewardAmount];
+    }
+
+    if(self.loadingAd)
+    {
+        self.loadingAd = NULL;
+    }
+    self.loadingAd = [[FBRewardedVideoAd new] initWithPlacementID:self.zoneId withUserID:userId withCurrency:self.rewardCurrencyForLoadingAd withAmount:[self.rewardAmountForLoadingAd intValue]];
+    self.loadingAd.delegate = self;
+    [self.loadingAd loadAd];
 }
 
 - (BOOL)hasAdAvailable
 {
-    return [[MPFacebookRouter sharedRouter] isAdAvailableForZoneId:self.zoneId];
+    if(!self.loadingAd)
+    {
+        return NO;
+    }
+    return [self.loadingAd isAdValid];
 }
 
 - (void)presentRewardedVideoFromViewController:(UIViewController *)viewController
@@ -58,26 +92,18 @@ static int sFacebookIdentifier = -1;
         FacebookInstanceMediationSettings *settings = [self.delegate instanceMediationSettingsForClass:[FacebookInstanceMediationSettings class]];
         NSString *customerId = [self.delegate customerIdForRewardedVideoCustomEvent:self];
         
-        [[MPFacebookRouter sharedRouter] presentRewardedVideoAdFromViewController:viewController customerId:customerId zoneId:self.zoneId settings:settings delegate:self];
+        self.displayedAd = self.loadingAd;
+        self.loadingAd = NULL;
+        self.rewardAmountForDisplayedAd = self.rewardAmountForLoadingAd;
+        self.rewardCurrencyForDisplayedAd = self.rewardCurrencyForLoadingAd;
+        self.isAdClosed = NO;
+        self.isRewardCallbackCalled = NO;
+        
+        [self.displayedAd showAdFromRootViewController:viewController];
     } else {
         MPLogInfo(@"Failed to show Facebook rewarded video: Facebook now claims that there is no available video ad.");
         NSError *error = [NSError errorWithDomain:MoPubRewardedVideoAdsSDKDomain code:MPRewardedVideoAdErrorNoAdsAvailable userInfo:nil];
         [self.delegate rewardedVideoDidFailToPlayForCustomEvent:self error:error];
-    }
-}
-
-- (void)handleCustomEventInvalidated
-{
-    [[MPFacebookRouter sharedRouter] clearDelegate:self];
-}
-
-- (void)handleAdPlayedForCustomEventNetwork
-{
-    // If we no longer have an ad available, report back up to the application that this ad expired.
-    // We receive this message only when this ad has reported an ad has loaded and another ad unit
-    // has played a video for the same ad network.
-    if (![self hasAdAvailable]) {
-        [self.delegate rewardedVideoDidExpireForCustomEvent:self];
     }
 }
 
@@ -91,78 +117,195 @@ static int sFacebookIdentifier = -1;
     return sFacebookIdentifier;
 }
 
-#pragma mark - MPFacebookRouterDelegate
+#pragma mark - FBRewardedVideoAdDelegate
 
-- (void)facebookVideoCompleted:(NSString *)rewardItemKey rewardAmount:(NSNumber *)rewardAmount
+/*!
+ @method
+ 
+ @abstract
+ Sent after the FBRewardedVideoAd object has finished playing the video successfully.
+ Reward the user on this callback.
+ 
+ @param rewardedVideoAd An FBRewardedVideoAd object sending the message.
+ */
+- (void)rewardedVideoAdComplete:(FBRewardedVideoAd *)rewardedVideoAd
 {
 }
-- (void)facebookVideoAdServerSuccess:(NSString *)rewardItemKey rewardAmount:(NSNumber *)rewardAmount
+
+/*!
+ @method
+ 
+ @abstract
+ Sent if server call to publisher's reward endpoint returned HTTP status code 200.
+ 
+ @param rewardedVideoAd An FBRewardedVideoAd object sending the message.
+ */
+- (void)rewardedVideoAdServerSuccess:(FBRewardedVideoAd *)rewardedVideoAd
 {
-    NSString *currencyType = kMPRewardedVideoRewardCurrencyTypeUnspecified;
-    if (rewardItemKey) {
-        currencyType = rewardItemKey;
-    }
-    NSNumber *amount = @(kMPRewardedVideoRewardCurrencyAmountUnspecified);
-    if(rewardAmount)
+    self.isRewardCallbackCalled = YES;
+    if(self.displayedAd && self.displayedAd == rewardedVideoAd)
     {
-        amount = rewardAmount;
+        MPRewardedVideoReward *reward = [[MPRewardedVideoReward alloc] initWithCurrencyType:self.rewardCurrencyForDisplayedAd amount:self.rewardAmountForDisplayedAd];
+        
+        if(self.isAdClosed == YES)
+        {
+            self.isAdClosed = NO;
+            self.isRewardCallbackCalled = NO;
+            self.displayedAd = NULL;
+        }
+        [self.delegate rewardedVideoShouldRewardUserForCustomEvent:self reward:reward];
     }
-    MPRewardedVideoReward *reward = [[MPRewardedVideoReward alloc] initWithCurrencyType:currencyType amount:amount];
-    [self.delegate rewardedVideoShouldRewardUserForCustomEvent:self reward:reward];
-}
-- (void)facebookVideoAdServerFailed:(NSString *)rewardItemKey rewardAmount:(NSNumber *)rewardAmount
-{
-    NSString *currencyType = kMPRewardedVideoRewardCurrencyTypeUnspecified;
-    if (rewardItemKey) {
-        currencyType = rewardItemKey;
-    }
-    NSNumber *amount = @(kMPRewardedVideoRewardCurrencyAmountUnspecified);
-    if(rewardAmount)
+    else
     {
-        amount = rewardAmount;
+        //add this code for FAN bug : this callback is called by all loaded ads. It should be called only by the displayed ad
+        if(self.loadingAd == rewardedVideoAd)
+        {
+            //set other as expired so that MoPub will load them again on the next loadAd call
+            [self.delegate rewardedVideoDidExpireForCustomEvent:self];
+        }
     }
-    MPRewardedVideoReward *reward = [[MPRewardedVideoReward alloc] initWithCurrencyType:currencyType amount:amount];
-    [self.delegate rewardedVideoFailedToRewardUserForCustomEvent:self reward:reward];
 }
 
-- (void)facebookWillShow
+/*!
+ @method
+ 
+ @abstract
+ Sent if server call to publisher's reward endpoint did not return HTTP status code 200
+ or if the endpoint timed out.
+ 
+ @param rewardedVideoAd An FBRewardedVideoAd object sending the message.
+ */
+- (void)rewardedVideoAdServerFailed:(FBRewardedVideoAd *)rewardedVideoAd
 {
-    [self.delegate rewardedVideoWillAppearForCustomEvent:self];
+    self.isRewardCallbackCalled = YES;
+    if(self.displayedAd && self.displayedAd == rewardedVideoAd)
+    {
+        MPRewardedVideoReward *reward = [[MPRewardedVideoReward alloc] initWithCurrencyType:self.rewardCurrencyForDisplayedAd amount:self.rewardAmountForDisplayedAd];
+        
+        if(self.isAdClosed == YES)
+        {
+            self.isAdClosed = NO;
+            self.isRewardCallbackCalled = NO;
+            self.displayedAd = NULL;
+        }
+        [self.delegate rewardedVideoFailedToRewardUserForCustomEvent:self reward:reward];
+    }
+    else
+    {
+        //add this code for FAN bug : this callback is called by all loaded ads. It should be called only by the displayed ad
+        if(self.loadingAd == rewardedVideoAd)
+        {
+            //set other as expired so that MoPub will load them again on the next loadAd call
+            [self.delegate rewardedVideoDidExpireForCustomEvent:self];
+        }
+    }
 }
 
-- (void)facebookDidShow
+/*!
+ @method
+ 
+ @abstract
+ Sent immediately before the impression of an FBRewardedVideoAd object will be logged.
+ 
+ @param rewardedVideoAd An FBRewardedVideoAd object sending the message.
+ */
+- (void)rewardedVideoAdWillLogImpression:(FBRewardedVideoAd *)rewardedVideoAd
 {
-    [self.delegate rewardedVideoDidAppearForCustomEvent:self];
+    if(self.displayedAd && self.displayedAd == rewardedVideoAd)
+    {
+        [self.delegate rewardedVideoWillAppearForCustomEvent:self];
+        [self.delegate rewardedVideoDidAppearForCustomEvent:self];
+    }
 }
 
-- (void)facebookWillHide
+/*!
+ @method
+ 
+ @abstract
+ Sent after an FBRewardedVideoAd object has been dismissed from the screen, returning control
+ to your application.
+ 
+ @param rewardedVideoAd An FBRewardedVideoAd object sending the message.
+ */
+- (void)rewardedVideoAdDidClose:(FBRewardedVideoAd *)rewardedVideoAd
 {
-    [self.delegate rewardedVideoWillDisappearForCustomEvent:self];
+    self.isAdClosed = YES;
+    if(self.displayedAd && self.displayedAd == rewardedVideoAd)
+    {
+        if(self.isRewardCallbackCalled == YES)
+        {
+            self.isAdClosed = NO;
+            self.isRewardCallbackCalled = NO;
+            self.displayedAd = NULL;
+        }
+        [self.delegate rewardedVideoDidDisappearForCustomEvent:self];
+    }
+    
 }
 
-- (void)facebookDidHide
+/*!
+ @method
+ 
+ @abstract
+ Sent immediately before an FBRewardedVideoAd object will be dismissed from the screen.
+ 
+ @param rewardedVideoAd An FBRewardedVideoAd object sending the message.
+ */
+- (void)rewardedVideoAdWillClose:(FBRewardedVideoAd *)rewardedVideoAd
 {
-    [self.delegate rewardedVideoDidDisappearForCustomEvent:self];
+    if(self.displayedAd && self.displayedAd == rewardedVideoAd)
+    {
+        [self.delegate rewardedVideoWillDisappearForCustomEvent:self];
+    }
 }
 
-- (void)facebookWillLeaveApplication
+/*!
+ @method
+ 
+ @abstract
+ Sent when an ad has been successfully loaded.
+ 
+ @param rewardedVideoAd An FBRewardedVideoAd object sending the message.
+ */
+- (void)rewardedVideoAdDidLoad:(FBRewardedVideoAd *)rewardedVideoAd
 {
-    [self.delegate rewardedVideoWillLeaveApplicationForCustomEvent:self];
+    if(self.loadingAd && self.loadingAd == rewardedVideoAd)
+    {
+        [self.delegate rewardedVideoDidLoadAdForCustomEvent:self];
+    }
 }
 
-- (void)facebookFetchCompleted
+/*!
+ @method
+ 
+ @abstract
+ Sent after an ad has been clicked by the person.
+ 
+ @param rewardedVideoAd An FBRewardedVideoAd object sending the message.
+ */
+- (void)rewardedVideoAdDidClick:(FBRewardedVideoAd *)rewardedVideoAd
 {
-    [self.delegate rewardedVideoDidLoadAdForCustomEvent:self];
+    if(self.displayedAd && self.displayedAd == rewardedVideoAd)
+    {
+        [self.delegate rewardedVideoDidReceiveTapEventForCustomEvent:self];
+    }
 }
 
-- (void)facebookDidClick
+/*!
+ @method
+ 
+ @abstract
+ Sent after an FBRewardedVideoAd fails to load the ad.
+ 
+ @param rewardedVideoAd An FBRewardedVideoAd object sending the message.
+ @param error An error object containing details of the error.
+ */
+- (void)rewardedVideoAd:(FBRewardedVideoAd *)rewardedVideoAd didFailWithError:(NSError *)error
 {
-    [self.delegate rewardedVideoDidReceiveTapEventForCustomEvent:self];
-}
-
-- (void)facebookDidFailWithError:(NSError *)error
-{
-    [self.delegate rewardedVideoDidFailToLoadAdForCustomEvent:self error:error];
+    if(self.loadingAd && self.displayedAd == rewardedVideoAd)
+    {
+        [self.delegate rewardedVideoDidFailToLoadAdForCustomEvent:self error:error];
+    }
 }
 
 @end
